52fdbcca9da53c2bdad38e2c7eaaffb53a5a18ad
Hyeopgeon Lee <soft1318@naver.com>
2025-04-03 14:28:02 +0900

    하둡 3.3.6 버전으로 업그레이드

--- src/main/java/combiner/IPCount2TimeCheck1.java
+++ src/main/java/combiner/IPCount2TimeCheck1.java
@@ -1,6 +1,7 @@
 package combiner;
 
 import lombok.extern.log4j.Log4j;
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -12,98 +13,48 @@ import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
 /**
- * 컴바이너가 적용된 맵리듀스 실행시간 체크
+ * Combiner가 적용된 맵리듀스 실행 시간 측정용 드라이버
  */
 @Log4j
-public class IPCount2TimeCheck1 extends Configuration implements Tool {
+public class IPCount2TimeCheck1 extends Configured implements Tool {
 
-    // 맵리듀스 실행 함수
     public static void main(String[] args) throws Exception {
-
-        // 파라미터는 분석할 파일(폴더)과 분석 결과가 저장될 파일(폴더)로 2개를 받음
         if (args.length != 2) {
-            log.info("분석할 폴더(파일) 및 분석결과가 저장될 폴더를 입력해야 합니다.");
+            log.error("사용법: <입력경로> <출력경로>");
             System.exit(-1);
-
         }
 
-        // 컴바이너가 적용된 맵리듀스 실행 전 시간
-        long startTime = System.nanoTime();
-        int exitCode = ToolRunner.run(new IPCount2TimeCheck1(), args);
+        long start = System.currentTimeMillis();
+        int exitCode = ToolRunner.run(new Configuration(), new IPCount2TimeCheck1(), args);
+        long end = System.currentTimeMillis();
 
-        // 컴바이너가 적용된 맵리듀스 실행 완료 시간
-        long endTime = System.nanoTime();
+        log.info("Combiner 적용 MapReduce 작업 실행 시간: " + (end - start) + "ms");
 
-        log.info("Time : " + (endTime - startTime) + "ns");
         System.exit(exitCode);
-
-    }
-
-    @Override
-    public void setConf(Configuration configuration) {
-
-        // App 이름 정의
-        configuration.set("AppName", "Combiner Time Test");
-    }
-
-    @Override
-    public Configuration getConf() {
-
-        // 맵리듀스 전체에 적용될 변수를 정의할 때 사용
-        Configuration conf = new Configuration();
-
-        // 변수 정의
-        this.setConf(conf);
-
-        return conf;
     }
 
     @Override
     public int run(String[] args) throws Exception {
+        Configuration conf = getConf();
+        conf.set("AppName", "Combiner Time Test");
 
-        Configuration conf = this.getConf();
         String appName = conf.get("AppName");
+        log.info("실행 중인 Job 이름: " + appName);
 
-        log.info("appName : " + appName);
-
-        // 맵리듀스 실행을 위한 잡 객체를 가져오기
-        // 하둡이 실행되면, 기본적으로 잡 객체를 메모리에 올림
         Job job = Job.getInstance(conf);
-
-        // 맵리듀스 잡이 시작되는 main함수가 존재하는 파일 설정
         job.setJarByClass(IPCount2TimeCheck1.class);
+        job.setJobName(appName);
 
-        // 맵리듀스 잡 이름 설정, 리소스 매니저 등 맵리듀스 실행 결과 및 로그 확인할 때 편리함
-        job.setJobName("Combiner IP Count2");
-
-        // 분석할 폴더(파일) -- 첫번째 파라미터
         FileInputFormat.setInputPaths(job, new Path(args[0]));
-
-        // 분석 결과가 저장되는 폴더(파일) -- 두번째 파라미터
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-        // 맵리듀스의 맵 역할을 수행하는 Mapper 자바 파일 설정
         job.setMapperClass(IPCount2Mapper.class);
-
-        // 맵리듀스의 리듀스 역할을 수행하는 Reducer 자바 파일 설정
         job.setReducerClass(IPCount2Reducer.class);
-
-        // 미니 리듀서라 부르며, Combiner 수행 설정
-        // 보통 Reducer 역할을 수행하는 객체를 바인딩 함
         job.setCombinerClass(IPCount2Reducer.class);
 
-        // 분석 결과가 저장될때 사용될 키의 데이터 타입
         job.setOutputKeyClass(Text.class);
-
-        // 분석 결과가 저장될때 사용될 값의 데이터 타입
         job.setOutputValueClass(IntWritable.class);
 
-        // 맵리듀스 실행
-        boolean success = job.waitForCompletion(true);
-
-        return (success ? 0 : 1);
+        return job.waitForCompletion(true) ? 0 : 1;
     }
-
 }
-
-
